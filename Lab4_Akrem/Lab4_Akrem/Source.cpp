e968c127e35bb54ca5c2ba08793fa67739766424
Owen Oliveira <119921389+OwenAtConestoga@users.noreply.github.com>
2023-02-15 20:55:31 -0500

    Add files via upload

---
+++ Lab4_Akrem/Lab4_Akrem/Source.cpp
@@ -0,0 +1,15 @@
+// Group 13 - Owen - Ryan - Matteo
+// Winter 2023
+// Lab 03 - PROG71020-23W-Object Oriented Programming
+// 
+// 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl;
+
+	return 0;
+	
+}
